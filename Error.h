6e1a8e9be842c6ba6b8931dab132cf12e2d0ccfd
César Torres <shortanemoia@protonmail.com>
2021-06-03 19:32:57 +0200

    Neo: Add Error header
    This header contains various error codes used by the library.

---
+++ Error.h
@@ -0,0 +1,45 @@
+#pragma once
+#include <errno.h>
+
+namespace neo
+{
+    enum class Error
+    {
+        OperationNotPermitted = EPERM,
+        NoSuchEntity = ENOENT,
+        NoSuchProcess = ESRCH,
+        IOError = EIO,
+        NoSuchFileOrAddress = ENXIO,
+        ArgumentListTooLong = E2BIG,
+        ExecFormatError = ENOEXEC,
+        BadFileNumber = EBADF,
+        NoChildProcesses = ECHILD,
+        TryAgain = EAGAIN,
+        OutOfMemory = ENOMEM,
+        PermissionDenied = EACCES,
+        BadAddress = EFAULT,
+        BlockDeviceRequired = ENOTBLK,
+        DeviceOrResourceBusy = EBUSY,
+        FileExists = EEXIST,
+        CrossDeviceLink = EXDEV,
+        NoSuchDevice = ENODEV,
+        NotADirectory = ENOTDIR,
+        IsADirectory = EISDIR,
+        InvalidArgument = EINVAL,
+        FileTableOverflow = ENFILE,
+        TooManyOpenFiles = EMFILE,
+        NotATypewriter = ENOTTY,
+        TextFileBusy = ETXTBSY,
+        FileTooLarge = EFBIG,
+        NoSpaceLeftOnDevice = ENOSPC,
+        IllegalSeek = ESPIPE,
+        ReadOnlyFilesystem = EROFS,
+        TooManyLinks = EMLINK,
+        BrokenPipe = EPIPE,
+        ArgumentOutOfDomain = EDOM,
+        MathResultNotRepresentable = ERANGE,
+        _CustomErrorStart = 1024,
+        EndOfFile = 1025
+    };
+}
+using neo::Error;
