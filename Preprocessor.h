dde3b3e2b4466bca5cb90bb4e42c5b4b34a9c6c4
Iori Torres <shortanemoia@protonmail.com>
2021-11-07 18:47:20 +0100

    Preprocessor: implement a way to stringify the values of enums

--- Preprocessor.h
+++ Preprocessor.h
@@ -17,4 +17,21 @@
 
 #pragma once
 
+//The following method to implement a FOR_EACH macro is extracted from https://www.scs.stanford.edu/~dm/blog/va-opt.html
+#define __PARENS ()
+#define __EXPAND(...) __EXPAND4(__EXPAND4(__EXPAND4(__EXPAND4(__VA_ARGS__))))
+#define __EXPAND4(...) __EXPAND3(__EXPAND3(__EXPAND3(__EXPAND3(__VA_ARGS__))))
+#define __EXPAND3(...) __EXPAND2(__EXPAND2(__EXPAND2(__EXPAND2(__VA_ARGS__))))
+#define __EXPAND2(...) __EXPAND1(__EXPAND1(__EXPAND1(__EXPAND1(__VA_ARGS__))))
+#define __EXPAND1(...) __VA_ARGS__
+#define __FOR_EACH(macro, ...)                                    \
+  __VA_OPT__(__EXPAND(__FOR_EACH_HELPER(macro, __VA_ARGS__)))
+#define __FOR_EACH_HELPER(macro, a1, ...)                         \
+  macro(a1)                                                       \
+  __VA_OPT__(__FOR_EACH_AGAIN __PARENS (macro, __VA_ARGS__))
+#define __FOR_EACH_AGAIN() __FOR_EACH_HELPER
+
 #define STRINGIFY(x) #x
+
+#define __ENUM_CASE(name) case name: return #name;
+
