ea7dd2c905acd3c1e94f7ec47e39be16b6205e1e
César Torres <shortanemoia@protonmail.com>
2021-11-14 21:52:57 +0100

    ResultOrError: add template specializations for when TResult and TError are void

--- ResultOrError.h
+++ ResultOrError.h
@@ -30,10 +30,13 @@ namespace neo
 
         ~ResultOrError()
         {
-            if (m_has_error)
-                ((TError*)&m_storage)->~TError();
-            else
-                ((TResult*)&m_storage)->~TResult();
+            if constexpr(IsTrivial<TResult> && IsTrivial<TError>)
+            {
+                if (m_has_error)
+                    ((TError *) &m_storage)->~TError();
+                else
+                    ((TResult *) &m_storage)->~TResult();
+            }
         }
 
         ResultOrError& operator=(ResultOrError&& other) = delete;
@@ -120,8 +123,116 @@ namespace neo
         }
 
     private:
-        char m_storage[max(sizeof(TResult), sizeof(TError))] { false };
-        bool m_has_error { true };
+        char m_storage[max(sizeof(TResult), sizeof(TError))] { };
+        bool m_has_error { };
     };
+    
+    template<typename TError>
+    class ResultOrError<void, TError>
+    {
+    public:
+        ResultOrError() = delete;
+    
+        ~ResultOrError()
+        {
+            if constexpr(!IsTrivial<TError>)
+            {
+                if (m_has_error)
+                    ((TError *) &m_storage)->~TError();
+            }
+        }
+    
+        ResultOrError& operator=(ResultOrError&& other) = delete;
+    
+        constexpr ResultOrError& operator=(const ResultOrError& other)
+        {
+            if (this == &other)
+                return *this;
+        
+            if constexpr (IsTriviallyCopyable<TError>)
+            {
+                __builtin_memcpy(m_storage, other.m_storage, sizeof(m_storage));
+            }
+            else
+            {
+                if (other.m_has_error)
+                    *reinterpret_cast<TError*>(&m_storage) = *reinterpret_cast<TError*>(&other.m_storage);
+            }
+            m_has_error = other.m_has_error;
+        }
+    
+        constexpr ResultOrError(ResultOrError&& other) :
+                m_has_error(other.m_has_error)
+        {
+            if (other.m_has_error)
+                *reinterpret_cast<TError*>(&m_storage) = move(*reinterpret_cast<TError*>(&other.m_storage));
+        }
+    
+        constexpr ResultOrError(const TError& other) :
+                m_has_error(true)
+        {
+            new (&m_storage) TError(other);
+        }
+    
+        constexpr ResultOrError(TError&& other) :
+                m_has_error(true)
+        {
+            new (&m_storage) TError(move(other));
+        }
+    
+        [[nodiscard]] constexpr bool has_error() const
+        {
+            return m_has_error;
+        }
+    
+        [[nodiscard]] constexpr bool is_result() const
+        {
+            return !m_has_error;
+        }
+    
+        [[nodiscard]] constexpr TError& error()
+        {
+            VERIFY(has_error());
+            return *reinterpret_cast<TError*>(&m_storage);
+        }
+
+    private:
+        char m_storage[sizeof(TError)] { };
+        bool m_has_error { };
+    };
+    
+    template<>
+    class ResultOrError<void, void>
+    {
+    public:
+        constexpr ResultOrError() = delete;
+        constexpr ~ResultOrError() = default;
+        constexpr ResultOrError(ResultOrError&& other) = default;
+        constexpr ResultOrError& operator=(ResultOrError&& other) = default;
+        constexpr ResultOrError& operator=(const ResultOrError& other) = default;
+        
+        constexpr ResultOrError(bool success) : m_has_error(!success)
+        {}
+        
+        [[nodiscard]] constexpr bool is_error() const
+        {
+            return m_has_error;
+        }
+        
+        [[nodiscard]] constexpr bool is_success() const
+        {
+            return !m_has_error;
+        }
+    
+    private:
+        bool m_has_error { };
+    };
+    
+    template<typename TResult, typename TError>
+    using ResultOr = ResultOrError<TResult, TError>;
+    template<typename TError>
+    using Result = ResultOrError<void, TError>;
 }
 using neo::ResultOrError;
+using neo::ResultOr;
+using neo::Result;
