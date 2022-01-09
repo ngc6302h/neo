d196380d7aea72bbae97dff0c4cfbff60cdaeac7
Iori Torres <shortanemoia@protonmail.com>
2022-01-09 12:26:22 +0100

    StringIterator: optimize utf-8 decoding

--- StringIterator.h
+++ StringIterator.h
@@ -36,24 +36,12 @@ namespace neo
         constexpr Utf8Char operator*() const
         {
             VERIFY(!is_end());
-            // TODO: Research how to make this branchless
-            int codepoint_size;
             const char* ptr = m_current;
-            if (((*ptr >> 7) & 1) == 0)
-                codepoint_size = 1;
-            else if (((*ptr >> 5) & 7) == 6)
-                codepoint_size = 2;
-            else if (((*ptr >> 4) & 0xF) == 0xE)
-                codepoint_size = 3;
-            else if (((*ptr >> 3) & 0x1F) == 0x1E)
-                codepoint_size = 4;
-            else
-                codepoint_size = 1;
-            Utf8Char codepoint;
+            int codepoint_size = __builtin_expect(((*ptr & 128) == 0), 1) ? 1 : __builtin_clz(~(*ptr & 0xf0) << 24);
+            Utf8Char codepoint = 0;
             switch (codepoint_size)
             {
-            case 1:
-                codepoint = ptr[0];
+                [[likely]] case 1 : codepoint = ptr[0];
                 break;
             case 2:
                 codepoint = (((u32)ptr[1]) & 0x3F) | (((u32)ptr[0]) & 0x1F) << 6;
@@ -61,13 +49,11 @@ namespace neo
             case 3:
                 codepoint = (((u32)ptr[2]) & 0x3F) | (((u32)ptr[1]) & 0x3F) << 6 | (((u32)ptr[0]) & 0xF) << 12;
                 break;
-            case 4:
-                codepoint = (((u32)ptr[3]) & 0x3F) | (((u32)ptr[2]) & 0x3F) << 6 | (((u32)ptr[1]) & 0x3F) << 12 | (((u32)ptr[0]) & 7) << 18;
+                [[unlikely]] case 4 : codepoint = (((u32)ptr[3]) & 0x3F) | (((u32)ptr[2]) & 0x3F) << 6 | (((u32)ptr[1]) & 0x3F) << 12 | (((u32)ptr[0]) & 7) << 18;
                 break;
             default:
                 VERIFY_NOT_REACHED();
             }
-            VERIFY(codepoint != 0);
             return codepoint;
         }
 
@@ -75,16 +61,8 @@ namespace neo
         {
             VERIFY(!is_end());
             const char* ptr = m_current;
-            if (((*ptr >> 7) & 1) == 0)
-                ptr++;
-            else if (((*ptr >> 5) & 7) == 6)
-                ptr += 2;
-            else if (((*ptr >> 4) & 0xF) == 0xE)
-                ptr += 3;
-            else if (((*ptr >> 3) & 0x1F) == 0x1E)
-                ptr += 4;
-            else
-                ptr++; // TODO: Verify this is correct behavior
+            int codepoint_size = __builtin_expect(((*ptr & 128) == 0), 1) ? 1 : __builtin_clz(~(*ptr & 0xf0) << 24);
+            ptr += codepoint_size;
             m_current = ptr;
             return *this;
         }
@@ -93,18 +71,7 @@ namespace neo
         {
             VERIFY(!is_end());
             auto prev = *this;
-            const char* ptr = m_current;
-            if (((*ptr >> 7) & 1) == 0)
-                ptr++;
-            else if (((*ptr >> 5) & 7) == 6)
-                ptr += 2;
-            else if (((*ptr >> 4) & 0xF) == 0xE)
-                ptr += 3;
-            else if (((*ptr >> 3) & 0x1F) == 0x1E)
-                ptr += 4;
-            else
-                ptr++; // TODO: verify this is correct behavior
-            m_current = ptr;
+            ++*this;
             return prev;
         }
 
@@ -129,17 +96,12 @@ namespace neo
 
         constexpr bool is_end() const
         {
-            return *m_current == 0 || m_current == m_end;
+            return m_current == m_end;
         }
 
         constexpr const char* ptr() const
         {
             return m_current;
-        }
-
-        constexpr bool operator!=(const StringIteratorContainer& other) const
-        {
-            return m_current != other.m_current;
         }
 
         constexpr bool operator==(const StringIteratorContainer& other) const
@@ -152,6 +114,7 @@ namespace neo
         const char* m_end;
         const char* m_current;
     };
+
     using StringIterator = StringIteratorContainer;
     using StringViewIterator = StringIteratorContainer;
 
