8306308c9100487e2db5d36f488927dd0ef38941
César Torres <shortanemoia@protonmail.com>
2022-01-18 20:20:29 +0100

    IterableUtil.h: use fold expression instead of type trait

--- IterableUtil.h
+++ IterableUtil.h
@@ -135,8 +135,8 @@ namespace neo
         return detail::_zip<TTupleA::size(), TTupleA, TTupleB>::zip(a, b);
     }
 
-    template<typename TTupleA, typename TTupleB, typename... TTuples>
-    [[nodiscard]] constexpr decltype(auto) zip(const TTupleA& first, const TTupleB& second, const TTuples&... tuples) requires(IsSameValue<size_t, TTupleA::size(), TTuples::size()>&&...)
+    template<typename TTupleA, typename TTupleB, typename... TTuples> requires((TTupleA::size() == TTuples::size())&&...)
+    [[nodiscard]] constexpr decltype(auto) zip(const TTupleA& first, const TTupleB& second, const TTuples&... tuples)
     {
         return zip(zip(first, second), tuples...);
     }
