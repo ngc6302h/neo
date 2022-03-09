5df390cf0aa9494742622d31aa443764281dce0e
Iori Torres <shortanemoia@protonmail.com>
2022-03-09 01:38:22 +0100

    Tree.h: add support for runtime-dependent arity

--- Tree.h
+++ Tree.h
@@ -18,6 +18,8 @@
 #pragma once
 #include "Concepts.h"
 #include "Assert.h"
+#include "Vector.h"
+
 namespace neo
 {
     template<typename T, size_t Arity = 2>
@@ -34,22 +36,22 @@ namespace neo
         {
         }
 
-        constexpr TreeNode*& left()
+        constexpr TreeNode*& left() requires(Arity == 2)
         {
             return m_children[0];
         }
 
-        constexpr TreeNode*& right()
+        constexpr TreeNode*& right() requires(Arity == 2)
         {
             return m_children[1];
         }
 
-        constexpr TreeNode const* const& left() const
+        constexpr TreeNode const* const& left() const requires(Arity == 2)
         {
             return m_children[0];
         }
 
-        constexpr TreeNode const* const& right() const
+        constexpr TreeNode const* const& right() const requires(Arity == 2)
         {
             return m_children[1];
         }
@@ -79,5 +81,44 @@ namespace neo
     private:
         T m_value {};
         TreeNode* m_children[Arity] {};
+    };
+
+    template<typename T>
+    class TreeNode<T, 0>
+    {
+    public:
+        explicit TreeNode(T const& value) :
+            m_value(value)
+        {
+        }
+
+        explicit TreeNode(T&& value) :
+            m_value(move(value))
+        {
+        }
+
+        constexpr T& value()
+        {
+            return m_value;
+        }
+
+        constexpr T const& value() const
+        {
+            return m_value;
+        }
+
+        constexpr TreeNode*& get(size_t children_index)
+        {
+            return m_children[children_index];
+        }
+
+        constexpr TreeNode const* const& get(size_t children_index) const
+        {
+            return m_children[children_index];
+        }
+
+    private:
+        T m_value;
+        Vector<TreeNode*> m_children;
     };
 }
