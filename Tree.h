684184baf93fe65721721854b8c725d4188f70a9
Iori Torres <shortanemoia@protonmail.com>
2022-11-25 16:33:30 +0100

    Tree.h: add Radix tree
    delete operation is still missing

--- Tree.h
+++ Tree.h
@@ -90,7 +90,7 @@ namespace neo
 
     private:
         T m_value {};
-        Vector<TreeNode*, Arity> m_children {};
+        Vector<TreeNode*> m_children {};
     };
 
     namespace detail
@@ -186,5 +186,184 @@ namespace neo
         TreeNode<detail::KeyValuePair<TKey, TValue>, 0> m_root;
     };
 
+    namespace detail
+    {
+        template<typename TIterator, typename TData, size_t InlineStorage>
+        struct Node
+        {
+            Node() = default;
+
+            Node(Node&& other) :
+                sequence(std::move(other.sequence)), data(std::move(other.data)), children(std::move(other.children))
+            {
+            }
+
+            Node(Vector<typename TIterator::type>&& seq, Optional<TData>&& dat, Vector<Node>&& ch) :
+                sequence(std::move(seq)), data(std::move(dat)), children(std::move(ch))
+            {
+            }
+
+            Node& operator=(Node&& other)
+            {
+                if (this == &other)
+                    return *this;
+
+                sequence = std::move(other.sequence);
+                data = std::move(other.data);
+                children = std::move(other.children);
+
+                return *this;
+            }
+
+            Vector<typename TIterator::type> sequence;
+            Optional<TData> data;
+            Vector<Node> children;
+        };
+    }
+
+    template<typename TIterator, typename TData, size_t InlineStorage = 0>
+    class RadixTree
+    {
+        using Node = detail::Node<TIterator, TData, InlineStorage>;
+        using sequence_type = typename TIterator::type;
+
+    public:
+        RadixTree(TIterator begin, TIterator end, Optional<TData>&& data) :
+            m_root { Vector<sequence_type>::from_range(begin, end), std::move(data), {} }
+        {
+        }
+
+    private:
+        void reorganize(Node& node, auto const& old_range_end)
+        {
+            auto break_node_from_here = node.sequence.begin();
+            while (break_node_from_here != old_range_end)
+                ++break_node_from_here;
+            ENSURE(break_node_from_here != node.sequence.end());
+            Node new_node { Vector<sequence_type>::from_range(break_node_from_here, node.sequence.end()), std::move(node.data), std::move(node.children) };
+            node.children = Vector<Node>();
+            node.children.append(std::move(new_node));
+            auto new_sequence = Vector<sequence_type>::from_range(node.sequence.begin(), break_node_from_here);
+            node.sequence = std::move(new_sequence);
+        }
+
+        void insert_internal(Node& node, TIterator begin, TIterator const& end, TData const& data)
+        {
+            auto begin_copy = begin;
+            auto seq_begin = node.sequence.begin();
+            auto seq_end = node.sequence.end();
+            for (; begin != end && seq_begin != seq_end && *begin == *seq_begin; begin++, seq_begin++)
+            {
+                if (begin == end || seq_begin == seq_end)
+                    break;
+            }
+            if (begin == end && seq_begin == seq_end) // if both reach the end, replace current value
+                node.data = data;
+            else if (seq_begin != seq_end && begin != end)
+            {
+                Node* closest_match = nullptr;
+                for (Node& n : node.children)
+                {
+                    size_t length = 0;
+                    auto n_begin = n.sequence.begin();
+                    auto begin_ = begin;
+                    while (n_begin != n.sequence.end() && begin_ != end && *begin_ == *n_begin)
+                        length++;
+                    if (length > 0)
+                        closest_match = &n;
+                }
+                if (closest_match != nullptr)
+                    insert_internal(*closest_match, begin, end, data);
+                else
+                {
+                    reorganize(node, seq_begin);
+                    insert_internal(node, begin_copy, end, data);
+                }
+            }
+            else
+                node.children.template construct(Vector<sequence_type>::from_range(begin, end), data, Vector<Node> {});
+        }
+
+        Optional<TData> get_internal(Node const& node, TIterator begin, TIterator const& end) const
+        {
+            if (begin == end)
+                return node.data;
+
+            for (auto& child : node.children)
+            {
+                if (*child.sequence.begin() == *begin)
+                {
+                    auto seq_begin = child.sequence.begin();
+                    while (seq_begin != child.sequence.end() && begin != end && *seq_begin == *begin)
+                    {
+                        seq_begin++;
+                        begin++;
+                    }
+                    return get_internal(child, begin, end);
+                }
+            }
+            return {};
+        }
+
+        void remove_internal(Node& node, TIterator begin, TIterator const& end)
+        {
+        }
+
+    public:
+        Optional<TData> get(TIterator begin, TIterator const& end) const
+        {
+            return get_internal(m_root, begin, end);
+        }
+
+        Optional<TData> get(auto const& sequence) const
+        {
+            return get_internal(m_root, sequence.begin(), sequence.end());
+        }
+
+        void insert(TIterator begin, TIterator const& end, TData const& data)
+        {
+            insert_internal(m_root, begin, end, data);
+        }
+
+        void insert(auto const& sequence, TData const& data)
+        {
+            insert_internal(m_root, sequence.begin(), sequence.end(), data);
+        }
+
+        void remove(TIterator begin, TIterator const& end)
+        {
+            remove_internal(m_root, begin, end);
+        }
+
+        void remove(auto const& sequence)
+        {
+            remove_internal(m_root, sequence.begin(), sequence.end());
+        }
+
+    private:
+
+        void debug_print_internal(Node& node, int depth)
+        {
+            __builtin_printf("%d ", depth);
+            for (auto k : node.sequence)
+                __builtin_printf("%c", k);
+            __builtin_printf("\n");
+            for (auto& c : node.children)
+                debug_print_internal(c, depth + 1);
+        }
+
+    public:
+        void debug_print()
+        {
+            __builtin_printf("-----\n");
+            debug_print_internal(m_root, 0);
+            __builtin_printf("-----\n");
+        }
+
+    private:
+        Node m_root;
+    };
+
 }
+using neo::RadixTree;
 using neo::Trie;
