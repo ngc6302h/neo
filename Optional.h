c948cc607421eb99ffcd684c39a3d45bb089d0a3
Iori Torres <shortanemoia@protonmail.com>
2022-11-26 04:34:43 +0100

    Optional: fixes

--- Optional.h
+++ Optional.h
@@ -1,18 +1,18 @@
 /*
-    Copyright (C) 2022  Iori Torres (shortanemoia@protonmail.com)
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+Copyright (C) 2022  Iori Torres (shortanemoia@protonmail.com)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
@@ -416,12 +416,12 @@ namespace neo
             return m_value;
         }
 
-        [[nodiscard]] constexpr T& value_or(T* fallback)
+        [[nodiscard]] constexpr T* value_or(T* fallback)
         {
             return m_value != nullptr ? m_value : fallback;
         }
 
-        [[nodiscard]] constexpr T const& value_or(T const* fallback) const
+        [[nodiscard]] constexpr T const* value_or(T const* fallback) const
         {
             return m_value != nullptr ? m_value : fallback;
         }
@@ -497,7 +497,7 @@ namespace neo
             return m_value;
         }
 
-        [[nodiscard]] constexpr T const& value_or(T const* fallback) const
+        [[nodiscard]] constexpr T const* value_or(T const* fallback) const
         {
             return m_value != nullptr ? m_value : fallback;
         }
