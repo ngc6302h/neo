729ae6edf84b1e0584a9358ad3c0f818aba54ba3
Iori Torres <shortanemoia@protonmail.com>
2022-11-26 03:48:27 +0100

    Tree.h: add default constructor

--- Tree.h
+++ Tree.h
@@ -1,24 +1,25 @@
 /*
-    Copyright (C) 2022  Iori Torres (shortanemoia@protonmail.com)
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as
-    published by the Free Software Foundation, either version 3 of the
-    License, or (at your option) any later version.
+Copyright (C) 2022  Iori Torres (shortanemoia@protonmail.com)
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 #include "Concepts.h"
 #include "Assert.h"
 #include "Vector.h"
 #include "Optional.h"
+#include "Array.h"
 
 namespace neo
 {
@@ -228,6 +229,11 @@ namespace neo
         using sequence_type = typename TIterator::type;
 
     public:
+        RadixTree() :
+            m_root { Vector<sequence_type>::create_with_capacity(1), {}, {} }
+        {
+        }
+
         RadixTree(TIterator begin, TIterator end, Optional<TData>&& data) :
             m_root { Vector<sequence_type>::from_range(begin, end), std::move(data), {} }
         {
